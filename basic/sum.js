28981c9764af43073f7fd74e55a02f3925139c93
immindy4527 <immindy4527@gmail.com>
2022-05-08 14:45:16 +0800

    用for迴圈

---
+++ basic/sum.js
@@ -0,0 +1,13 @@
+function sum(n) {
+let sum=0;
+for ( let i=0; i<=n; i++) {
+sum = sum +i
+}
+return sum;
+};
+
+// 1+2+3+4......
+console.log(sum(1)); //1
+console.log(sum(2)); //3
+console.log(sum(10));//55
+console.log(sum(100));//5050
