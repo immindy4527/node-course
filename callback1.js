d0e261570a419592258f66d5a69b0ea7b90e82d8
immindy4527 <immindy4527@gmail.com>
2022-05-14 15:29:27 +0800

    排版好看一點的

--- callback1.js
+++ callback1.js
@@ -4,23 +4,21 @@ let dt = new Date();
 console.log(`起床了 at ${dt.toISOString()}`);
 
 let doWork = function (job, timer, cb) {
-    setTimeout(() => {
-        let dt = new Date();
-        let result = `完成工作: ${job} at ${dt.toISOString()}`
-        cb(result);
-}, timer);
-// console.log(`在setTimeout 之後 ${job}`);
+  setTimeout(() => {
+    let dt = new Date();
+    let result = `完成工作: ${job} at ${dt.toISOString()}`;
+    cb(result);
+  }, timer);
+  // console.log(`在setTimeout 之後 ${job}`);
 };
 
 //刷牙 (3000) -> 吃早餐 (5000) -> 寫功課 (3000)
-doWork('刷牙', 3000, function(result){
-    console.log(result);
-doWork('吃早餐', 5000, function(result){
-    console.log(result);
-
-doWork('寫功課', 3000, function (result) {
+doWork("刷牙", 3000, function (result) {
   console.log(result);
+  doWork("吃早餐", 5000, function (result) {
+    console.log(result);
+    doWork("寫功課", 3000, function (result) {
+      console.log(result);
+    });
+  });
 });
-});
-});
-
