45eb2ffd77b306a480a1c31cc351e19baf3b8cb5
immindy4527 <immindy4527@gmail.com>
2022-05-15 11:57:30 +0800

    ㄚㄚㄚㄚ

---
+++ readfile/readfile1.js
@@ -0,0 +1,33 @@
+// const fs = require('fs');
+
+// fs.readFile('test11111.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     // 錯誤了
+//     console.log('喔喔喔，發生錯誤了');
+//     console.error(err);
+//   } else {
+//     // 因為沒有 err，所以是正確的
+//     console.log(data);
+//   }
+// });
+
+// Promise寫法
+const fs = require('fs')
+let p = new Promise((resolve, reject) => {
+  fs.readFile('text.txt','utf-8', (err, data) => {
+    if (err) {
+      // 錯誤
+      reject(err);
+    }else{
+      //否則正確
+      resolve(data);
+    }
+  })
+});
+
+
+p.then((result) => {
+  console.log(`讀取成功: ${result}`)
+}).catch((error) => {
+  console.error(`讀取失敗: `, error);
+});
