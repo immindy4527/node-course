f0891d110572e4e7f8c7131a746f62e8aeaabf22
immindy4527 <immindy4527@gmail.com>
2022-05-21 14:43:22 +0800

    新增環境設定檔'

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -3,15 +3,19 @@
 // mysql2 是一個第三方套件
 // npm i mysql2
 // 引用進來
+
+require('dotenv').config();
+
 const mysql = require('mysql2/promise');
+
 
 (async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'test',
+    host: process.env.DB_HOST,
+    port: process.env.DB_POST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
   });
 
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
---
+++ crawler/read.md
@@ -0,0 +1,3 @@
+## How to use this folder
+
+把 .env.example改成.env放在專案中
